67fe188f33c30aafe1006a47b932520ea8b84932
Ayaee <61822669+Ayaee@users.noreply.github.com>
2021-11-13 17:05:15 +0100

    testUn

---
+++ san_antonio.py
@@ -0,0 +1,36 @@
+# -*- coding: utf8 -*- # Pour afficher les accents
+
+quotes = [
+    "Ecoutez-moi, Monsieur Shakespeare, nous avons beau être ou ne pas être, nous sommes !",
+    "On doit pouvoir choisir entre s'écouter parler et se faire entendre."
+]
+
+characters = [
+    "alvin et les Chimpunks",
+    "Babar",
+    "betty boop",
+    "calimero",
+    "casper",
+    "le chat potté",
+    "Kirikou"
+]
+
+user_answer = input('Tapez entrée pour découvrir une autre citation ou B pour quitter le programme.')
+
+while
+
+if user_answer == "B":
+    pass
+else:
+    pass
+    #Montre une autre quote
+
+def get_random_item_in(my_list):
+    # A FAIRE : Donner un nombre aléatoire
+    item = my_list[0]                       #Donne une quote de la liste
+    print(item)                             #Montre une quote dans l'interpréteur
+    return "Programme terminé"              #Retour valeur
+
+print(get_random_item_in(quotes))
+
+# Afficher une quote aléatoire
