a5ad40273ae8bdad818523280c4756060da3ef66
Ayaee <afabre@artfx.fr>
2021-12-06 17:35:00 +0100

    addReq&Gitigno

--- generationQuoteChara/san_antonio.py
+++ generationQuoteChara/san_antonio.py
@@ -1,9 +1,10 @@
 # -*- coding: utf8 -*-
-import random
 import json
+import random
 
-# Donne le fichier Json et retourn la liste
-def readValues_fromJson(path, key):   # Lire les valeurs du fichier JSON
+
+# Donne le fichier Json et retourne une liste
+def readValues_fromJson(path, key):     # Lire les valeurs du fichier JSON
     values = []                         # Crée une liste vide
     with open(path) as f:               # Ouvre le fichier JSON avec mes objets
         data = json.load(f)             # Charge toutes les données contenue dans le fichier. data = entrée
@@ -11,21 +12,24 @@ def readValues_fromJson(path, key):   # Lire les valeurs du fichier JSON
             values.append(entry[key])
     return values                       # Retourn ma liste complete  
 
+
 # Donne json et retourne la liste
 def cleanString(sentences):
     cleaned = []
-    for sentence in sentences:              # Conserve les quotes dans une liste. Créer une liste vide et ajoute chaque phrase une par une
-        cleanSentence = sentence.strip()    # Nettoie les quotes des espaces blanc et ainsi de suite
-        cleaned.append(cleanSentence)       # Ne pas utiliser extend car il ajoute chaque lettre une par une !
+    for sentence in sentences:              # Stock les quotes dans une liste. Créer une liste vide et ajoute chaque phrase une par une
+        cleanSentence = sentence.strip()    # Nettoie les quotes des espaces blanc
+        cleaned.append(cleanSentence)       # Ne pas utiliser extend car il ajoute chaque lettre une par une
     return cleaned
-    
-# Retourne un item random de la liste
+
+
+# Retourne un item random dans la liste
 def randomItem(objectList):
     randNumb = random.randint(0, len(objectList) - 1)   # Donner un nombre aléatoire
     #item = myList[randNumb]                            # Donne une quote de la liste
     return objectList[randNumb]                         # Retour valeur
 
-# Retourne une valeur random du fichier json
+
+# Retourne une valeur random à partir du fichier json
 def randomValue(sourcePath, key):
     allValues = readValues_fromJson(sourcePath, key)
     cleanValues = cleanString(allValues)
@@ -36,17 +40,17 @@ def randomValue(sourcePath, key):
 #### QUOTES ####
 ################
 
-# Recueille des citations de San Antonio
+# Recueille les quotes de San Antonio
 def randomQuote():
-    #all_values = readValues_fromJson('quotes.json', 'quote')
     return randomValue('quotes.json', 'quote')
 
 ####################
 #### CHARACTERS ####
 ####################
 
+
+# Receuille les characters de Wikipédia
 def randomCharacter():
-    #all_values = readValues_fromJson('characters.json', 'character')
     return randomValue('characters.json', 'character')
 
 
@@ -54,19 +58,22 @@ def randomCharacter():
 #### INTERACTION ####
 #####################
 
+# Print une phrase random
 def printRandom_sentence():
     randQuote = randomQuote()
     randCharacter = randomCharacter()
     print(">>>> {} a dit : {}".format(randCharacter, randQuote))
-    
+
+
 def mainLoop():
     while True:
         printRandom_sentence()
-        message = ('Voulez-vous une autre citation ? Tapez sur [Entrer].'
-                   'Pour quitter, tapez [B].')
+        message = ('Voulez-vous voir une autre citation ? Tapez [Entrer].'
+                   'Pour sortir du programme, tapez [B].')
         choice = input(message).upper()
         if choice == 'B':
-            break
-        
-# Lancer la boucle!
-mainLoop()
+            break                                                           # Cela mettra fin à la boucle
+
+
+if __name__ == '__main__':
+    mainLoop()
