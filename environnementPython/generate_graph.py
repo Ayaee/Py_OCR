c4888e6a564516cf7fb5f40d9e1284067a56ae41
Ayaee <afabre@artfx.fr>
2022-09-16 13:10:09 +0200

    add summer works

---
+++ environnementPython/generate_graph.py
@@ -0,0 +1,9 @@
+import plistlib
+import matplotlib.pyplot as plt
+
+
+plt.figure()
+
+plt.plot([1, 2, 3, 4, 5])
+
+plt.show()
---
+++ environnementPython/projects/demo-app/demo.py
@@ -0,0 +1,3 @@
+import requests
+r = requests.get("http://wwW.example.com")
+print(r.status_code)
